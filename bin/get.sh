ffaec85e96e81cbe29511c3044486fcd578b374c
Vincent LAURENT <vince.laurent@gmail.com>
2026-05-22 22:46:45 +0200

    Erreur d'argument

--- bin/get.sh
+++ bin/get.sh
@@ -2,7 +2,7 @@ export LC_ALL=fr_FR.UTF-8
 
 curl https://docs.google.com/document/d/1sh-xkEMkNLGw7U8GacAPe4p6828jnDVApuIVeYb8VnI/mobilebasic > liste_complete.html
 
-cat liste_complete.html | grep -v "DOCTYPE" | tr -d "\n" | sed 's|</p>|#|g' | sed -r 's|<style>.*</style>||g' | sed -r 's|<javascript>.*</javascript>||g'  | sed -r 's|</?[a-z]+[^<>]*>||g' | sed -r 's/^.*zapperbollore@proton\.me//' | tr '#' "\n" | grep -r "Pour signer la tribune" | grep "," | grep -v window.set | grep -v function | sed 's/^39;//' | sed -r 's/[ \t]+$//' | sed -r 's/[ ]+/ /g' | sort > /tmp/liste_complete.txt.tmp
+cat liste_complete.html | grep -v "DOCTYPE" | tr -d "\n" | sed 's|</p>|#|g' | sed -r 's|<style>.*</style>||g' | sed -r 's|<javascript>.*</javascript>||g'  | sed -r 's|</?[a-z]+[^<>]*>||g' | sed -r 's/^.*zapperbollore@proton\.me//' | tr '#' "\n" | grep -v "Pour signer la tribune" | grep "," | grep -v window.set | grep -v function | sed 's/^39;//' | sed -r 's/[ \t]+$//' | sed -r 's/[ ]+/ /g' | sort > /tmp/liste_complete.txt.tmp
 
 if [ "$(md5sum /tmp/liste_complete.txt.tmp | cut -d " " -f 1)" != "$(md5sum liste_complete.txt | cut -d " " -f 1)" ]
 then
